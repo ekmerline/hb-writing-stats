bc6c631f4d24223ef91a7176a2ae46d9f146e3fc
ekmerline <emilykmerline@gmail.com>
2021-04-07 10:59:29 -0500

    created homepage.html and login.html, added navbar to base, and added routes for the homepage and login to server.py

--- server.py
+++ server.py
@@ -1,9 +1,23 @@
-"""Server for movie ratings app."""
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template, request, flash, session,
+                   redirect)
+from jinja2 import StrictUndefined
 from model import connect_to_db
 import crud
 
 app = Flask(__name__)
+
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def homepage():
+    """View homepage."""
+    return render_template('homepage.html', message='Welcome!')
+
+@app.route('/login')
+def login_page():
+    """View login."""
+    return render_template('login.html', message='Please log in!')
 
 @app.route('/api/projects')
 def get_projects():
