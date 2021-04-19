2f107632d9933ea843f31879a107c26ef2bb2b2e
ekmerline <emilykmerline@gmail.com>
2021-04-19 15:17:14 -0500

    Created login component and added conditional display for navbar based on login.

--- static/App.jsx
+++ static/App.jsx
@@ -1,19 +1,30 @@
 const { AppBar, Toolbar, Box, Typography } = MaterialUI;
 const { BrowserRouter, Link, Switch, Route } = ReactRouterDOM;
+const { useState } = React;
 
 const App = () => {
 
+    const [userID, setUserID ] = useState(null);
+
+    const verifyUser = () => {
+        setUserID(sessionStorage.getItem('user_id'));
+    }
     return (
         <BrowserRouter>
             <Box className="App">
-                <AppBar position="static" color="primary">
+                <AppBar position="static" color="secondary">
                     <Toolbar>
-                        <Typography>
-                            <Link to="/">Home</Link>
-                        </Typography>
-                        <Typography>
-                            <Link to="/login">Login</Link>
-                        </Typography>
+                        <Link to="/">Home</Link>
+                        {userID ? (
+                            <React.Fragment>
+                                <Link to="/new-project">New Project</Link>
+                                <Link to="/new-entry">New Entry</Link>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                <Link to="/login">Login</Link>
+                            </React.Fragment>
+                        )}
                     </Toolbar>
                 </AppBar>
                 <Box className="main">
@@ -28,7 +39,23 @@ const App = () => {
                         <Route path="/login">
                             <React.Fragment>
                                 <Box>
-                                    The login page!
+                                    <Login verifyUser={verifyUser}>
+
+                                    </Login>
+                                </Box>
+                            </React.Fragment>
+                        </Route>
+                        <Route path="/new-project">
+                            <React.Fragment>
+                                <Box>
+                                    The new project page!
+                                </Box>
+                            </React.Fragment>
+                        </Route>
+                        <Route path="/new-entry">
+                            <React.Fragment>
+                                <Box>
+                                    The new entry page!
                                 </Box>
                             </React.Fragment>
                         </Route>
