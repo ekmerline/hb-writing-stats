469ca5d2110c0e1e7b3ab0debb7e3d7e08b15ad3
ekmerline <emilykmerline@gmail.com>
2021-04-28 14:34:30 -0500

    Added checkbox menu list for projects, and updated filtering to allow multiple at once.

--- static/components/Project/EditProject.jsx
+++ static/components/Project/EditProject.jsx
@@ -1,16 +1,15 @@
 const { useState } = React;
 const { useHistory } = ReactRouterDOM;
 
-const EditProject = ({projectTypes, updateProjectsData, projectsData}) => {
+const EditProject = ({projectTypes, updateProjectsData, currentProject}) => {
     let history = useHistory();
     const [projectData, setProjectData] = useState({
-        project_id: '',
-        project_name: '',
-        project_description: '',
-        project_type_id: ''
+        project_name: currentProject['project_name'],
+        project_description: currentProject['project_description'],
+        project_type_id: currentProject['project_type_id']
     });
 
-    const { project_id, project_name, project_description, project_type_id } = projectData;
+    const { project_name, project_description, project_type_id } = projectData;
 
     const onChange = e => {
         const { name, value } = e.target;
@@ -20,23 +19,13 @@ const EditProject = ({projectTypes, updateProjectsData, projectsData}) => {
         })
     };
 
-    const onProjectChange = e => {
-        const currentProject = projectsData.filter(project => project['project_id'] === e.target.value)
-        setProjectData({
-            project_id: currentProject['project_id'],
-            project_name: currentProject['project_name'],
-            project_description: currentProject['project_description'],
-            project_type_id: currentProject['project_type_id']
-        })
-    }
-
     const updateProject = async () => {
         const newProject = {
             project_name: project_name,
             project_description: project_description,
             project_type_id: project_type_id
         }
-        fetch(`http://localhost:5000/api/project/${currentProjectID}`, {
+        fetch(`http://localhost:5000/api/project/${currentProject['project_id']}`, {
         method: 'PUT', 
         headers: {
         'Content-Type': 'application/json',
@@ -56,25 +45,6 @@ const EditProject = ({projectTypes, updateProjectsData, projectsData}) => {
 
     return (
         <Box>
-        <div>
-         <FormControl style={{minWidth: 200}}>
-                    <InputLabel id="project-label">Project</InputLabel>
-                    <Select
-                    labelId="project-label"
-                    value={project_id}
-                    name="project_id"
-                    onChange={e => onProjectChange(e)}
-                    >
-                    {projectsData.map((projectData, index) => 
-                        <MenuItem 
-                        key={index} 
-                        value={projectData['project_id']}>
-                            {projectData['project_name']}
-                        </MenuItem>
-                    )}
-                    </Select>
-                </FormControl>
-            </div>
         <ProjectForm
         onSubmit={updateProject}
         project_name={project_name}
